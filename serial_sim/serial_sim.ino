69c5f756af160fa337b90856407c4459c1be5f9e
dex <dex-k@users.noreply.github.com>
2020-12-27 20:10:20 +1100

    changed data format for time

--- serial_sim/serial_sim.ino
+++ serial_sim/serial_sim.ino
@@ -6,14 +6,14 @@ void setup() {
 void loop() {
   //init variables in loop scope
   static int altitude, temperature, pressure;
-  static float elapsedTime;
+  static unsigned long elapsedTime;
   
   //give random values in reasonable range for each quantity
   altitude = random(500), 
   temperature = random(15,26), 
   pressure = random(950,1051);
   //milliseconds since arduino started
-  elapsedTime = (float)millis() / 1000;
+  elapsedTime = millis();
 
   //log each, comma separated
   Serial.print(altitude);
@@ -22,7 +22,7 @@ void loop() {
   Serial.print(",");
   Serial.print(pressure);
   Serial.print(",");
-  Serial.println(elapsedTime, 2); //2 decimal places
+  Serial.println(elapsedTime);
 
   //random delay
   delay(random(1,50));
