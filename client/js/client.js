0b258bf7a2001d29c996f1b0c07c39c27d517bf2
dex <dex-k@users.noreply.github.com>
2020-12-27 20:10:09 +1100

    trying to get apexcharts working

--- client/js/client.js
+++ client/js/client.js
@@ -1,11 +1,15 @@
+//open websocket connection
 const ws = new WebSocket("ws://localhost:5678/");
 
+//basic websocket handlers
 ws.onopen = function () {
+    //change status indicator
     document.getElementById('status').classList.replace("disconnected", "connected");
     document.getElementById('status').innerText = "OPEN";
 }
 
 ws.onclose = function() {
+    //change status indicator
     document.getElementById('status').classList.replace("connected", "disconnected");
     document.getElementById('status').innerText = "CLOSED";
 }
@@ -15,7 +19,7 @@ ws.onmessage = function(e) {
 };
 
 
-
+//text spans for each value
 const htmlValues = document.getElementsByClassName("ws-value");
 
 function wsMessageHandler(e) {
@@ -23,4 +27,31 @@ function wsMessageHandler(e) {
     wsValues = e.data.split(','); //extract data from ws content
 
     wsValues.map( (val, i) => htmlValues[i].innerText = val);
+
+    // altitudeChart.appendData([
+    //     wsValues[0], // altitude
+    //     wsValues[3] // milliseconds elapsed
+    // ])
 }
+
+//Apex charts
+var altitudeChartOptions = {
+    chart: {
+        type: 'line'
+    },
+    series: [{
+        name: 'altitude',
+        data: [ // [time, altitude]
+            [0,0],
+            [1,1.5],
+            [2,2]
+        ]
+    }],
+    xaxis: {
+    //    type: 'numeric', 
+    }
+  }
+  
+  var altitudeChart = new ApexCharts(document.querySelector("#altitude-chart"), altitudeChartOptions);
+  
+  altitudeChart.render();
