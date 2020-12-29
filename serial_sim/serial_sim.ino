5974c33e4315d768d9b0ddfed51e803beef8ee24
dex <dex-k@users.noreply.github.com>
2020-12-29 11:45:56 +1100

    added smooth curve test

--- serial_sim/serial_sim.ino
+++ serial_sim/serial_sim.ino
@@ -5,15 +5,17 @@ void setup() {
 
 void loop() {
   //init variables in loop scope
-  static int altitude, temperature, pressure;
+  static float altitude, temperature, pressure;
   static unsigned long elapsedTime;
   
   //give random values in reasonable range for each quantity
   altitude = random(500), 
   temperature = random(15,26), 
-  pressure = random(950,1051);
+//  pressure = random(950,1051);
   //milliseconds since arduino started
   elapsedTime = millis();
+  
+  pressure = 100*(float)sin((float)elapsedTime/10000);
 
   //log each, comma separated
   Serial.print(altitude);
@@ -24,7 +26,7 @@ void loop() {
   Serial.print(",");
   Serial.println(elapsedTime);
 
-  delay(1000); //1 second
+//  delay(1000); //1 second
   //random delay
-  //delay(random(1,50));
+  delay(random(1,50));
 }
