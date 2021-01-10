0c877aadf936ab1fd3e524c0a3fe01a56107b073
Knoll787 <noel.woods@protonmail.com>
2021-01-10 11:53:29 +1100

    better format for transmitting data

--- src/serial_sim/serial_sim.ino
+++ src/serial_sim/serial_sim.ino
@@ -6,30 +6,23 @@ void setup() {
 void loop() {
   //init variables in loop scope
   static float altitude, temperature, pressure, velocity;
-  static unsigned long elapsedTime;
+  static unsigned long elapsedTime = millis();
   
   //give random values in reasonable range for each quantity
   altitude = random(500) / 2 + 16;
   temperature = random(15,26) * 7 - 3;
   velocity = random(15,26);
-//  pressure = random(950,1051);
-  //milliseconds since arduino started
-  elapsedTime = millis();
-  
   pressure = 100*(float)sin((float)elapsedTime/10000);
-
+  
   //log each, comma separated
-  Serial.print(altitude);
-  Serial.print(",");
-  Serial.print(temperature);
-  Serial.print(",");
-  Serial.print(pressure);
-  Serial.print(",");
-  Serial.println(velocity);
-  Serial.print(",");
-  Serial.println(elapsedTime);
+  Serial.print(String(altitude)        + ","+
+               String(temperature)     + ","+
+               String(velocity)        + ","+
+               String(pressure)         + ","+
+               String(elapsedTime)     + "\n" );
 
-//  delay(1000); //1 second
+
+  //  delay(1000); //1 second
   //random delay
   delay(random(1,50));
 }
