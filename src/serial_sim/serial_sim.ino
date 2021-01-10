7148ca78fb7e44d7be2676e2bd605e9347f677e9
Knoll787 <noel.woods@protonmail.com>
2021-01-10 14:15:46 +1100

    Changed time units to hunderedths of a second

--- src/serial_sim/serial_sim.ino
+++ src/serial_sim/serial_sim.ino
@@ -6,14 +6,13 @@ void setup() {
 void loop() {
   //init variables in loop scope
   static float altitude, temperature, pressure;
-  static unsigned long elapsedTime;
+  static float elapsedTime;
   
   //give random values in reasonable range for each quantity
   altitude = random(500), 
   temperature = random(15,26), 
-//  pressure = random(950,1051);
   //milliseconds since arduino started
-  elapsedTime = millis();
+  elapsedTime = millis()/10;
   
   pressure = 100*(float)sin((float)elapsedTime/10000);
 
