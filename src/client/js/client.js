decc20fb1e6dd797be5494305badbc10d4572cc5
Knoll787 <noel.woods@protonmail.com>
2021-01-19 21:50:43 +1100

    Guage that confroms to bootstrap sizing

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -164,80 +164,51 @@ const defaultChartOptions = {
 	},
 
 	guage: {
-		series: [80],
+		series: [76],
 		chart: {
 			type: 'radialBar',
-			toolbar: { show: false },
+			//offsetY: -20,
+			sparkline: { enabled: false },
 			foreColor: '#ccc'
 		},
-
 		plotOptions: {
 			radialBar: {
-				startAngle: -135,
-				endAngle: 135,
-				hollow: {
-					margin: 0,
-					size: '70%',
-					background: '#fff',
-					//imageOffsetX: 0,
-					//imageOffsetY: 0,
-					position: 'front',
-					dropShadow: {
-						enabled: true,
-						top: 3,
-						left: 0,
-						blur: 4,
-						opacity: 0.24
-					}
-				},
-				track: {
-					//background: '#fff',
-					strokeWidth: '67%',
-					//margin: 0, // margin is in pixels
-					dropShadow: {
-						enabled: true,
-						top: -3,
-						left: 0,
-						blur: 4,
-						opacity: 0.35
-					}
-					},
-				dataLabels: {
-					show: true,
-					name: {
-						offsetY: -10,
-						show: true,
-						color: '#888',
-						fontSize: '17px'
-					},
-					value: {
-						formatter: function(val) {
-							
-							return 10;
-						},
-					color: '#111',
-					fontSize: '36px',
-					show: true,
-					}
+				startAngle: -90,
+				endAngle: 90,
+			track: {
+				background: "#e7e7e7",
+				strokeWidth: '97%',
+				margin: 5, // margin is in pixels
+				dropShadow: {
+					enabled: true,
+					top: 2,
+					left: 0,
+					color: '#999',
+					opacity: 1,
+					blur: 2
+				}
+			},
+			dataLabels: {
+				name: { show: false },
+				value: {
+					//offsetY: -2,
+					fontSize: '22px'
 				}
 			}
+			}
 		},
-
 		fill: {
-				type: 'gradient',
-				gradient: {
-					shade: 'dark',
-					type: 'horizontal',
-					shadeIntensity: 0.5,
-					gradientToColors: ['#ABE5A1'],
-					inverseColors: true,
-					opacityFrom: 1,
-					opacityTo: 1,
-					stops: [0, 100]
-				}
+			type: 'gradient',
+			gradient: {
+				shade: 'light',
+				shadeIntensity: 0.4,
+				inverseColors: false,
+				opacityFrom: 1,
+				opacityTo: 1,
+				stops: [0, 50, 53, 91]
+			},
 		},
-		stroke: { lineCap: 'round' },
-		labels: ['G'],
+		labels: ['Average Results'],
 	}
 }
 
