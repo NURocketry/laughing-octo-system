92b9015bada402c7f2bad909eed813cccb673132
Knoll787 <noel.woods@protonmail.com>
2021-01-20 09:30:07 +1100

    Properly sized speedo

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -164,51 +164,77 @@ const defaultChartOptions = {
 	},
 
 	guage: {
-		series: [76],
+		series: [75],
 		chart: {
 			type: 'radialBar',
-			//offsetY: -20,
-			sparkline: { enabled: false },
-			foreColor: '#ccc'
+			toolbar: { show: false },
+			foreColor: '#ccc',
 		},
 		plotOptions: {
 			radialBar: {
-				startAngle: -90,
-				endAngle: 90,
-			track: {
-				background: "#e7e7e7",
-				strokeWidth: '97%',
-				margin: 5, // margin is in pixels
-				dropShadow: {
-					enabled: true,
-					top: 2,
-					left: 0,
-					color: '#999',
-					opacity: 1,
-					blur: 2
-				}
-			},
-			dataLabels: {
-				name: { show: false },
+				startAngle: -135,
+				endAngle: 135,
+				offsetY: -5,
+				hollow: {
+					margin: 0,
+					size: '50%',
+					background: '#fff',
+					position: 'front',
+					dropShadow: {
+						enabled: true,
+						top: 3,
+						left: 0,
+						blur: 4,
+						opacity: 0.24
+					}
+				},
+				track: {
+					background: '#fff',
+					strokeWidth: '67%',
+					margin: 0, // margin is in pixels
+					dropShadow: {
+						enabled: true,
+						top: -3,
+						left: 0,
+						blur: 4,
+						opacity: 0.35
+					}
+				},
+
+				dataLabels: {
+					show: true,
+					name: {
+						offsetY: -10,
+						show: true,
+						color: '#888',
+						fontSize: '17px'
+					},
 				value: {
-					//offsetY: -2,
-					fontSize: '22px'
+					formatter: function(val) {
+						return parseInt(val);
+					},
+					color: '#111',
+					fontSize: '36px',
+					show: true,
+					}
 				}
-			}
 			}
 		},
 		fill: {
 			type: 'gradient',
 			gradient: {
-				shade: 'light',
-				shadeIntensity: 0.4,
-				inverseColors: false,
+				shade: 'dark',
+				type: 'horizontal',
+				shadeIntensity: 0.5,
+				gradientToColors: ['#ABE5A1'],
+				inverseColors: true,
 				opacityFrom: 1,
 				opacityTo: 1,
-				stops: [0, 50, 53, 91]
-			},
+				stops: [0, 100]
+			}
 		},
-		labels: ['Average Results'],
+		stroke: { lineCap: 'round' },
+		labels: ['Percent']
 	}
 }
 
