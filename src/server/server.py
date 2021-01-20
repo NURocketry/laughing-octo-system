c02e4d1287c03a1b54733c3f2e610c0cf0354a24
tnthelps <r-c_heli@hotmail.com>
2021-01-20 20:18:52 +1100

    included "await asyncio.sleep()" which fixes the issue of it not running on windows machines, the sleep has been tested down to 0.01 seconds with no issues

--- src/server/server.py
+++ src/server/server.py
@@ -4,7 +4,7 @@ import websockets
 
 # Serial stuff
 
-#Port Selection
+# Port Selection
 port = '/dev/ttyACM0'
 portSelection = input('Use default port: /dev/ttyACM0? Y/n: ')
 if portSelection == 'n':
@@ -13,29 +13,36 @@ if portSelection == 'n':
 baud = 115200
 ser = serial.Serial(port, baud, timeout=1)
 
-#Handle income serial data and send to client via websockets
+
+# Handle income serial data and send to client via websockets
 async def serial_stream(websocket, path):
-    readCount = 0 #Used to limit the amount of data sent to site
+    readCount = 0  # Used to limit the amount of data sent to site
     while True:
         if ser.isOpen():
 
             # read serial content, strip trailing /r/n, decode bytes to string
-            serial_content = ser.readline().strip().decode('utf-8')  
+            serial_content = ser.readline().strip().decode('utf-8')
 
-            if len(serial_content) and readCount % 10  == 0: # make sure we don't send a blank message (happens) and limits render time
-                print(serial_content) #logging/debugging
+            if len(serial_content) and readCount == 9:  # make sure we don't send a blank message (happens) and limits render time
+                print(serial_content)  # logging/debugging
 
                 await websocket.send(serial_content)
+
+                # for some reason including the sleep makes it work on windows, if it causes and issues the sleep
+                # time can be decreased
+                # please note that it is in seconds, not millisecond
+                await asyncio.sleep(1)
+
                 readCount = 0
             readCount += 1
-        else: 
+        else:
             # if connection has closed for some reason, try and open it again indefinitely
             # ... objectively a bad idea but hacky solution to allow arduino resets during testing
             # potentially will need to be properly implemented in case connection with rocket is lost and regained mid flight
             ser.open()
 
+
 start_server = websockets.serve(serial_stream, "localhost", 5678)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
-
