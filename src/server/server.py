4145b1825017ffde5d99bcd3151610234b59ebbf
dex <dex-k@users.noreply.github.com>
2021-01-29 00:04:34 +1100

    i think i finally fixe the timing issue !!!!!

--- src/server/server.py
+++ src/server/server.py
@@ -57,23 +57,23 @@ async def file_stream(websocket, path):
     read_count = 0 # rate limit sent data
     reference_time = time.time() # seconds since epoch for accurate playback
 
-    for _line in f:
+    for line in (l.strip() for l in f): # iterate over each line with trailing newlines removed
         
-        csv_content = f.readline().strip() # read line discarding the newline character
+        # csv_content = f.readline().strip() # read line discarding the newline character
 
-        if len(csv_content) < 2 : continue # dont send empty lines
+        if len(line) < 2 : continue # dont send empty lines
         # dont merge these two if statements because itll fuckup the $read_count rate limiting
 
         if (read_count % 10) == 0: # limits render time
 
-            print(csv_content) #logging/debugging
+            print(line) #logging/debugging
 
             '''
             TODO: fix timing issues, probably something to do with offset not accounting for execution time, or differences
             in reacount rate limiting for serial streaming/reading from file.
             '''
 
-            timestamp = float( csv_content.split(',')[0] ) # exract time value from csv line
+            timestamp = float( line.split(',')[0] ) # exract time value from csv line
             current_time = time.time()
             delta = current_time - reference_time # difference between the python program's time and the live time
             offset = timestamp - delta # amount by which the program is ahead of schedule
@@ -83,7 +83,7 @@ async def file_stream(websocket, path):
             if offset > 0: #if the loop is running faster than incoming data, wait to catch up
                 time.sleep(offset)
             
-            await websocket.send(csv_content)
+            await websocket.send(line)
         
         read_count += 1 
     
@@ -93,10 +93,10 @@ async def file_stream(websocket, path):
 # requires windows-1252 encoding instead of UTF-8 because superscript 2's arent ecoded as utf8 atm
 with open(filepath, mode, encoding = 'windows-1252' ) as f: #ensures f.close() is called at the end
     if mode == 'r':
-        # next(f) # discard csv column labels
+        next(f) # discard csv column labels
         start_server = websockets.serve(file_stream, "localhost", 5678)
     elif mode == 'w':
-        f.write("Time (s), Altitude (m), Velocity (m/s), Acceleration (m/s^2), Air temperature (°C), Air pressure (mbar)\n") # add column labels
+        f.write("Time (s), Altitude (m), Velocity (m/s), Acceleration (m/s^2), Air temperature (oC), Air pressure (mbar)\n") # add column labels
         start_server = websockets.serve(serial_stream, "localhost", 5678)
 
     asyncio.get_event_loop().run_until_complete(start_server)
