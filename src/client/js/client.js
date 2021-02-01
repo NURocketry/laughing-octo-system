6cf94481e05415190a8674b51fb65fe32c063beb
josh55525 <c3304206@uon.edu.au>
2021-02-01 20:40:53 +1100

    Axis client.js file.

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -4,19 +4,26 @@ const ws = new WebSocket('ws://localhost:5678/');
 //basic websocket handlers
 ws.onopen = function () {
     //change status indicator
-	document.querySelector('#status').classList.replace('disconnected', 'connected');
-    document.querySelector('#status').innerText = 'OPEN';
+	document.querySelector('.status').classList.replace('disconnected', 'connected');
+    document.querySelector('.status').innerText = 'OPEN';
+	document.querySelector(".status");
+	let status = document.querySelectorAll(".status");
+	for (let item of status) 
+		item.style.color= "green";
 }
 
 
 ws.onclose = function() {
     //change status indicator
-	document.querySelector('#status').classList.replace('connected', 'disconnected');
-    document.querySelector('#status').innerText = 'CLOSED';
+	document.querySelector('.status').classList.replace('connected', 'disconnected');
+    document.querySelector('.status').innerText = 'CLOSED';
+	let status = document.querySelectorAll(".status");
+	for (let item of status) 
+		item.style.color= "red";
+		
 }
 
 ws.onmessage = function(e) { 
-    console.log("New message:")
     console.log(e)
     wsMessageHandler(e);
 };
@@ -25,56 +32,71 @@ function wsMessageHandler(e) {
     let wsValues = e.data.split(',').map( x => parseFloat(x) ); //extract data from ws content and convert to number
     
     let namedData = { 
-        'time': 			[wsValues[0]],
-        'altitude': 		[wsValues[1]],
-        'velocity': 		[wsValues[2]],
-        'acceleration': 	[wsValues[3]],
-        'temperature': 		[wsValues[4]],
-        'pressure': 		[wsValues[5]]
+        'time': 	      [wsValues[0]],
+        'altitude': 	  [wsValues[1]],
+        'velocity': 	  [wsValues[2]],
+        'acceleration': [wsValues[3]],
+        'temperature': 	[wsValues[4]],
+        'pressure': 	  [wsValues[5]]
     }
 
-    const htmlValues = document.querySelectorAll('.ws-value'); //get HTMLCollection of text spans for each value to be displayed
-    
+
+    //push ws data onto chart data array and handles statistics
+    addData(namedData);
+
+
+	//get HTMLCollection of text spans for each value to be displayed
+	const htmlValuesTelemetry = document.querySelectorAll('.ws-value'); 
+	const htmlValuesStats = document.querySelectorAll('.ws-stat'); 
+
     /**
      * this works but relies on the id being _exactly_ the same as the object label, which is fine for the moment but
      * could cause issues. more robust solution below. I'm leaving this one uncommented for performance and because 
      * it works atm.
      */
-    for ( let item of htmlValues ) 
+	
+	
+	//Appends named data with all the minimums statistics
+    for ( let item of htmlValuesStats ){
+		
+		//i.e Concert minVelocity => velocity
+		// so it is in the same format as in datasets
+		let formatted = item.id.slice(3).toLowerCase();
+
+		//Adds data to namedData for easy display
+		namedData[item.id] = datasets[formatted]['stats']['min'];
+		if(item.id.slice(0,3) == 'min') {
+			namedData[item.id] = datasets[formatted]['stats']['min'];
+		}
+		else if(item.id.slice(0,3) == 'max') {
+			namedData[item.id] = datasets[formatted]['stats']['max'];
+		}
+	}
+
+    for ( let item of htmlValuesTelemetry )
         item.innerText = namedData[item.id]; //extract data based on id
-
-    /**
-     * Better solution based on matching the object label to the html ID. means it doesnt need to be a perfect
-     * match but containing the string is enough.
-     */
-    // for ( let item of htmlValues ) { //each matching DOM element
-    //     for ( let label in namedData ) { //each entry in namedData
-    //         if ( item.id.includes(label) ) { //does the html ID contain the label somewhere *CASE SENSITIVE*
-    //             item.innerText = namedData[label]; //set the value from the corresponding item
-    //         }
-    //     }
-    // }
-
-    //push ws data onto chart data array
-    addData(namedData);
+	
 
     //re-draw charts accordingly
     update();
 
     //cut off datapoints to keep at 10 max and redraw
-    //trimData(namedData, 50);
+    trimData(namedData, 50);
 };
 
 
 //Templates for the different graphs options
 //Documentation: https://apexcharts.com/docs/installation/
-//TODO: Remove repeated code 
  
-const defaultChartOptions = {
+let defaultChartOptions = {
 	line: { 
-		series: [], 
+		series: [
+            {
+              name: "Series 1",
+              data: []
+            }
+          ],
 		noData: { text: "No Data"}, 
-
 		chart: {
 			type: 'line',
 			foreColor: '#ccc',
@@ -119,9 +141,13 @@ const defaultChartOptions = {
 	},
 
 	area: { 
-		series: [], 
+		series: [
+            {
+              name: "Series 1",
+              data: []
+            }
+          ],
 		noData: { text: "No Data"}, 
-
 		chart: {
 			type: 'area',
 			foreColor: '#ccc',
@@ -163,119 +189,129 @@ const defaultChartOptions = {
 			type: 'numeric',
 		},
 
-	},
-
-	/*
-	//Can only take a single value not a series
-	guage: {
-		series: [], 
-		noData: { text: "No Data"}, 
-
-		chart: {
-			type: "radialBar",
-			foreColor: '#ccc',
-			toolbar: { show: false },
-            animations: {
-                enabled: false,
-                easing: 'smooth',
-                dynamicAnimation: { speed: 1000 }
-            },
-
-		},
-
-		series: [67],
-		plotOptions: {
-
-		radialBar: {
-			startAngle: -135,
-			endAngle: 135,
-
-			track: {
-				background: '#333',
-				startAngle: -135,
-				endAngle: 135,
-			},
-			dataLabels: { enabled: true}
-		}
 	}
-	*/
 }
 
 
 //Specific Implementaions of different charts
 /**
  * @param data : array of datapoints
+ * @param stats: statistics for that data element 
  * @param hasChart : if the data has an associated chart
  * @param id : DOM id of the chart div
  * @param options : the apexCharts options for the chart
  */
 let datasets = {
     altitude: {
-        data: [],
+        series: [
+            {
+              name: "Series",
+              data: []
+            }
+          ],
+        // data: [],
+		stats: {min: null, max: null},
         hasChart: true,
         id: '#altitude-chart',
         options: {
             ...defaultChartOptions.area, 
             ...{ //rest will override defaults
-				colors: ['#d45087'],
+				colors: ['#9b5de5'],
                 title: { text: 'Altitude' }
             }
         }
     },
     temperature: {
-        data: [],
+        series: [
+            {
+              name: "Series",
+              data: []
+            }
+          ],
+		stats: {min: null, max: null},
         hasChart: true,
         id: '#temperature-chart',
         options: {
             ...defaultChartOptions.line, 
             ...{ //rest will override defaults
-				colors: ['#ffa600'],
+				colors: ['#f15bb5'],
                 title: { text: 'Temperature' }
             }
         }
     },
     pressure: {
-        data: [],
+        series: [
+            {
+              name: "Series",
+              data: []
+            }
+          ],
+		stats: {min: null, max: null},
         hasChart: true,
         id: '#pressure-chart',
         options: {
             ...defaultChartOptions.line, 
             ...{ //rest will override defaults
+				colors: ['#fee440'],
                 title: { text: 'Pressure' }
             }
         }
     },
     velocity: {
-        data: [],
+        series: [
+            {
+              name: "Series",
+              data: []
+            }
+          ],
+		stats: {min: null, max: null},
         hasChart: true,
         id: '#velocity-chart',
         options: {
             ...defaultChartOptions.area, 
             ...{ //rest will override defaults
-				colors: ['#ff7c43'],
+				colors: ['#00bbf9'],
                 title: { text: 'Velocity' }
             }
         }
     },
     acceleration: {
-        data: [],
+        series: [
+            {
+              name: "Series",
+              data: []
+            }
+          ],
+		stats: {min: null, max: null},
         hasChart: true,
         id: '#acceleration-chart',
         options: {
             ...defaultChartOptions.area, 
             ...{ //rest will override defaults
-				colors: ['#ff1919'],
+				colors: ['#00f5d4'],
                 title: { text: 'Acceleration' }
             }
         }
     },
     time: {
-        data: [],
-        hasChart: false
+        series: [
+            {
+              name: "Series",
+              data: []
+            }
+          ],
+		stats: {min: null, max: null},
+		id: '#time-chart',
+        hasChart: true,
+        options: {
+            ...defaultChartOptions.area, 
+            ...{ //rest will override defaults
+				colors: ['#00f5d4'],
+                title: { text: 'Time' }
+            }
+        }
     }
 }
-
-
 
 
 /**
@@ -315,18 +351,29 @@ function render() {
  * data should be within arrays to allow for multiple datapoints to be added at once
  */
 function addData(dataObj) {
-    for ( let key in dataObj )
-        datasets[key].data.push( ...dataObj[key] ); // spread operator (...) 'splits' array into function arguments
+    for ( let key in dataObj ) {
+        
+        datasets[key].series[0].data.push([ dataObj["time"][0] ,dataObj[key]] ); // spread operator (...) 'splits' array into function arguments
+		//Determines the min/max for each element in the dataset
+		//used for the statistics page
+		let min = datasets[key].stats.min; 
+		let max = datasets[key].stats.max; 
+		if( dataObj[key] < min || min == null ) {
+			datasets[key].stats.min = dataObj[key][0]; 
+		}
+		if(dataObj[key] > max || max == null) {
+			datasets[key].stats.max = dataObj[key][0]; 
+		}
+
+	}
 }
 
 function trimData(dataObj, len) {
     let flag = false;
     for ( let key in dataObj ) {
-        // console.log(key, datasets[key].data.length);
-        if ( datasets[key].data.length > len ) {
+        if ( datasets[key].series[0].data.length > len ) {
 
-            // console.log(datasets[key].data.shift(), ' ');
-            datasets[key].data.shift()
+            datasets[key].series[0].data.shift()
             flag = true;
         }
     }
@@ -334,10 +381,27 @@ function trimData(dataObj, len) {
 }
 
 function update() {
-    for ( let s in datasets) { // each set in the datasets object
+    for ( var s in datasets) { // each set in the datasets object
         if (datasets[s].hasChart) {// if it contains a .chart property 
-            let chartName = s.replace(/^\w/, c => c.toUpperCase()); //capitalise first letter
-            charts[chartName].updateSeries([{ data: datasets[s].data }])
+            var chartName = s.replace(/^\w/, c => c.toUpperCase()); //capitalise first letter
+
+             try{
+                if(Object.keys(datasets[s].series[0].data).length != 0){
+                        charts[chartName].updateOptions({
+                            series: [{
+                                name : "Series",
+                                data: datasets[s].series[0].data
+                            }]
+                        },
+                        false,
+                        false,
+                        false);
+                    
+                }
+            }catch (e){
+                console.log(e);
+            }
+            
         }
     }
 }
---
+++ src/server/main.py
@@ -0,0 +1,182 @@
+import serial
+import asyncio
+import websockets
+import sys # commandline arguments
+import time # for logging
+import random
+from aiohttp import web #Web server
+
+
+if len(sys.argv) > 1: # filepath given, read
+
+    mode = 'r' # read
+    filepath = sys.argv[1] # filepath taken from commandline
+
+    print("Reading from local file @ %s" % filepath)
+
+else: # no filepath given, write
+
+    mode = 'a' # append mode so data is not overwritten in case of existing filename 
+
+    filepath = "../data/" + time.strftime("%Y-%m-%dT%H%M%SZ", time.gmtime()) + " launch.csv" # ISO8601 compliant time filename
+
+    print("Logging flight data to local file: '%s'" % filepath)
+
+    # init required serial stuff
+    #Port Selection
+    port = input('port: ') if input('use default port: COM3? Y/n: ') == 'n' else 'COM3'
+
+    baud = 115200
+    ser = serial.Serial(port, baud, timeout=1) # establish serial connection
+
+USERS = set() #All active web socket connection
+
+#Broadcast messages to all web socket connected
+async def notify_state(STATE):
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = STATE
+        await asyncio.wait([user.send(message) for user in USERS])
+
+#Add new web socket user
+async def register(websocket):
+    print("REGISTER")
+    USERS.add(websocket)
+
+#Remove web socket user
+async def unregister(websocket):
+    USERS.remove(websocket)
+# if a commandline argument is passed at all, it is assumed to be a filepath and the program enters read-from-file mode
+# if no commandline arguments are given the program enters write mode for logging and attempts to connect to the serial port
+
+
+async def counter(websocket, path):
+    await register(websocket)
+    try:
+        # For some reason have to read a message?????????
+        async for message in websocket:
+            print(message)
+    finally:
+        await unregister(websocket)
+
+# Handle income serial data and send to client via websockets
+async def serial_stream():
+    read_count = 0 #Used to limit the amount of data sent to site
+    # while True:
+    #     if ser.isOpen():
+
+    #         # read serial content, strip trailing /r/n, decode bytes to string
+    #         serial_content = ser.readline().strip().decode('utf-8')
+
+    #         if len(serial_content): # make sure we don't send a blank message (happens) and 
+
+    #             # f.writelines(serial_content) # log to file with trailing newline(!) (no rate limiting for logs)
+
+    #             if read_count % 10  == 0: # limits render time
+          
+    #                 print(serial_content) #logging/debugging
+    #                 await notify_state(serial_content)
+              
+    #                 # for some reason including the sleep makes it work on windows, if it causes and issues the sleep
+    #                 # time can be decreased
+    #                 # please note that it is in seconds, not millisecond
+    #                 await asyncio.sleep(0)
+
+    #         read_count += 1
+
+    #     else: 
+    #         # if connection has closed for some reason, try and open it again indefinitely
+    #         # ... objectively a bad idea but hacky solution to allow arduino resets during testing
+    #         # potentially will need to be properly implemented in case connection with rocket is lost and regained mid flight
+    #         ser.open()
+
+    a = 0
+    while True:
+        b = str(random.randint(1, 1000))
+        c = str(random.randint(1, 1000))
+        d = str(random.randint(1, 1000))
+        e = str(random.randint(1, 1000))
+        g = str(random.randint(1, 1000))
+
+        stateString = str(a) + "," + b + "," + c + "," + d + "," + e+ ","+ g
+        f.writelines(stateString)
+        await notify_state(stateString)
+        await asyncio.sleep(0.5)
+        a = a +1 
+
+#handle file data and send to client via websockets
+async def file_stream(websocket, path):
+
+    read_count = 0 # rate limit sent data
+
+    reference_time = time.time() # seconds since epoch for accurate playback
+
+    for line in (l.strip() for l in f): # iterate over each line with trailing newlines removed
+        
+        if len(line) < 2 : continue # dont send empty lines
+        # dont merge these two if statements because itll fuckup the $read_count rate limiting
+
+        if (read_count % 10) == 0: # limits render time
+
+            print(line) #logging/debugging
+
+            # the only reason these variables are explicitly split up and defined is for debugging/logging
+            # doubt it's importance (performance-wise) to combine the statements and I figure it's done under
+            # the hood during interpretation anyway
+
+            timestamp = float( line.split(',')[0] ) # exract time value from csv line
+
+            current_time = time.time() # take a wild fucking guess what this is xox
+
+            delta = current_time - reference_time # difference between the python program's time and the live time
+            offset = timestamp - delta # amount by which the program is ahead of schedule
+            
+            # debugging
+            # print("-> %0.3f (%0.3f = %0.3f - %0.3f)" % (offset, delta, current_time, reference_time) ) 
+
+            if offset > 0: # if the loop is running faster than incoming data, wait to catch up
+                time.sleep(offset) # sleep in seconds
+            
+            # second data from file
+            await websocket.send(line)
+        
+        read_count += 1 # increment rate limiter
+    
+    print("\nFinished replaying launch data from '%s'" % filepath)
+
+
+#Web server to publish html and other client side resources
+#Web server to publish web page contents this includes resources like css and js files
+async def index(request):
+    return web.FileResponse('../client/liveData.html')
+
+app = web.Application()
+app.add_routes([web.get('/', index)])
+app.router.add_static('/', path='../client/')
+
+
+# requires windows-1252 encoding instead of UTF-8 because superscript 2's arent ecoded as utf8 atm
+# TODO ensure we're ready to make utf-8 encoding standard for all NuRocketry stuff (and pure ascii block preffered)
+# global f
+with open(filepath, mode, encoding = 'windows-1252' ) as f: # 'with' is important as it ensures file is closed even on an exception
+    # READ i.e. replaying from file
+    if mode == 'r':
+        next(f) # discard csv column labels by skipping first line
+        # TODO server handling to be updated to work with Josh's improvements (including both windows and multiple connections)
+        start_server = websockets.serve(counter, "localhost", 5678)
+
+    # WRITE i.e. logging data from serial
+    elif mode == 'a': # append mode ensures data is not overwritten in case of a file name mishap
+        # add column labels
+        f.write("Time (s), Altitude (m), Velocity (m/s), Acceleration (m/s^2), Air temperature (oC), Air pressure (mbar)\n")
+        # TODO server handling to be updated to work with Josh's improvements (including both windows and multiple connections)
+        start_server = websockets.serve(counter, "localhost", 5678)
+
+    # TODO server handling to be updated to work with Josh's improvements (including both windows and multiple connections)
+    asyncio.ensure_future(start_server)
+    asyncio.ensure_future(serial_stream())
+    asyncio.ensure_future(web.run_app(app,port=8080)) #Web server running on port 8080
+
+    #Make run in infinite loop
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
+    loop.close()
