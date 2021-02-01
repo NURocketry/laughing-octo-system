83cc82cbda5ab0d206ff17d2708212a04743023d
dex <dex-k@users.noreply.github.com>
2021-02-01 21:15:14 +1100

    data doesn't need to be in an array all the time

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -31,12 +31,12 @@ function wsMessageHandler(e) {
     let wsValues = e.data.split(',').map( x => parseFloat(x) ); //extract data from ws content and convert to number
     
     let namedData = { 
-        'time': 	      [wsValues[0]],
-        'altitude': 	  [wsValues[1]],
-        'velocity': 	  [wsValues[2]],
-        'acceleration': [wsValues[3]],
-        'temperature': 	[wsValues[4]],
-        'pressure': 	  [wsValues[5]]
+        'time':         wsValues[0],
+        'altitude':     wsValues[1],
+        'velocity':     wsValues[2],
+        'acceleration': wsValues[3],
+        'temperature': 	wsValues[4],
+        'pressure':     wsValues[5]
     }
 
 
